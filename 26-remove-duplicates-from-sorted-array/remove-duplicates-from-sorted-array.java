831106fe0c786de235ee00c3f809ec6b8e388e4d
Delvin joseph <124490000+delvinjoseph13@users.noreply.github.com>
2023-10-24 19:19:16 +0530

    Time: 1 ms (96.58%) | Memory: 43.5 MB (91.46%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -3,11 +3,10 @@ class Solution {
         int left=1;
         for(int i=1;i<nums.length;i++){
             if(nums[i]!=nums[i-1]){
-               nums[left]=nums[i];
-               left++;
+                nums[left]=nums[i];
+                left++;
             }
         }
         return left;
-        
     }
 }
