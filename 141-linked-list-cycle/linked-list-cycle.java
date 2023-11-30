76750ec55d851a3acef041ea041818a2300a03f3
Delvin joseph <124490000+delvinjoseph13@users.noreply.github.com>
2023-11-30 19:11:47 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (20.69%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head==null || head.next==null) return false;
+
+        ListNode fastPointer=head;
+        ListNode slowPointer=head;
+
+        while(fastPointer!=null && fastPointer.next!=null){
+            slowPointer=slowPointer.next;
+            fastPointer=fastPointer.next.next;
+
+            if(fastPointer==slowPointer){
+                return true;
+            }
+        }
+        return false;
+    }
+}
