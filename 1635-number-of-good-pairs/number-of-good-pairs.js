07b6fa72827dbf4357766abf69054c07e4a1cfad
Delvin joseph <124490000+delvinjoseph13@users.noreply.github.com>
2024-12-17 16:55:53 +0530

    Time: 1 ms (45.52%) | Memory: 48.4 MB (90.67%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.js
+++ 1635-number-of-good-pairs/number-of-good-pairs.js
@@ -3,13 +3,13 @@
  * @return {number}
  */
 var numIdenticalPairs = function(nums) {
-    let sum=0;
-   for(let i=0;i<=nums.length;i++){
-    for(let j=i+1;j<=nums.length;j++){
-          if(nums[i]==nums[j]){
-            sum+=1
-          }
+let sum=0;
+for(let i=0;i<nums.length;i++){
+    for(let j=i+1;j<nums.length;j++){
+        if(nums[i]==nums[j]){
+            sum++;
+        }
     }
-   }
-   return sum; 
+}
+return sum;
 };
