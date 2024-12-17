416676c94bbfb9c389d03125f008307d46c61906
Delvin joseph <124490000+delvinjoseph13@users.noreply.github.com>
2024-12-17 11:27:34 +0530

    Time: 1 ms (37.57%) | Memory: 51.4 MB (62.17%) - LeetSync

---
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+    let runningSum=[];
+    runningSum[0]=nums[0];
+
+    for(let i=1;i<nums.length;i++){
+        runningSum[i]=runningSum[i-1]+nums[i];
+    }
+    return runningSum;
+};
