8889e4e9ada85fc0e2d6d081351cfde7abdfb231
Delvin joseph <124490000+delvinjoseph13@users.noreply.github.com>
2024-12-19 11:33:32 +0530

    Time: 0 ms (100.00%) | Memory: 50.5 MB (45.35%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.js
+++ 782-jewels-and-stones/jewels-and-stones.js
@@ -4,19 +4,11 @@
  * @return {number}
  */
 var numJewelsInStones = function(jewels, stones) {
-    let map=new Map();
-    for(let i=0;i<stones.length;i++){
-        if(map.has(stones[i])){
-            map.set(stones[i],map.get(stones[i])+1)
-        }else{
-            map.set(stones[i],1)
-        }
-    }
-
+    let set =new Set(jewels);
     let count=0;
-    for(let j=0;j<jewels.length;j++){
-        if(map.has(jewels[j])){
-            count+= map.get(jewels[j])
+    for(let i of stones){
+        if(set.has(i)){
+            count++;
         }
     }
     return count;
