6ba0880a490f51bd19863647e385916a50c016a7
Delvin joseph <124490000+delvinjoseph13@users.noreply.github.com>
2024-12-20 10:30:29 +0530

    Time: 2 ms (92.63%) | Memory: 51.9 MB (10.06%) - LeetSync

--- 2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.js
+++ 2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.js
@@ -3,15 +3,21 @@
  * @param {number} k
  * @return {number}
  */
+
+// TC-O(n)
+// SC-O(n)
 var countKDifference = function(nums, k) {
+    let map=new Map();
     let count=0;
-    for(let i=0;i<nums.length-1;i++){
-        for(let j=i+1;j<nums.length;j++){
-            if(Math.abs(nums[j]-nums[i])==k){
-                count++;
-            }
+
+    for(let i=0;i<nums.length;i++){
+        if(map.has(nums[i]-k)){
+            count+=map.get(nums[i]-k);
         }
+        if(map.has(nums[i]+k)){
+            count+=map.get(nums[i]+k);
+        }
+        map.set(nums[i],(map.get(nums[i])||0)+1)
     }
     return count;
-}
-
+};
