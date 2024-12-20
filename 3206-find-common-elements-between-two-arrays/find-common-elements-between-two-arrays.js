24a3c49b9588465885d6f558d67c87e54b4bec75
Delvin joseph <124490000+delvinjoseph13@users.noreply.github.com>
2024-12-20 10:36:03 +0530

    Time: 11 ms (32.36%) | Memory: 54.9 MB (50.18%) - LeetSync

--- 3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.js
+++ 3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.js
@@ -3,28 +3,21 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
-
-// TC-(n*m);
-// SC-O(n)
 var findIntersectionValues = function(nums1, nums2) {
-   let set1=new Set(nums1)
-   let set2=new Set(nums2)
-   let answer1=0;
-   let answer2=0;
+    let set1=new Set(nums1);
+    let set2=new Set(nums2);
+    let answer1=0;
+    let answer2=0;
 
-   for(let num of nums1){
-      if(set2.has(num)){
-        answer1++;
-      }
-   }
-
-   for(let num of nums2){
-    if(set1.has(num)){
-        answer2++;
+    for(let i=0;i<nums1.length;i++){
+        if(set2.has(nums1[i])){
+            answer1++;
+        }
     }
-   }
-
-    
+    for(let j=0;j<nums2.length;j++){
+        if(set1.has(nums2[j])){
+            answer2++;
+        }
+    }
     return [answer1,answer2]
-}
-
+};
