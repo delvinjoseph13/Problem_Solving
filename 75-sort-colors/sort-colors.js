41712bfa245b7fa45eef5938e7d70f93da0d7a76
Delvin joseph <124490000+delvinjoseph13@users.noreply.github.com>
2024-12-22 22:16:06 +0530

    Time: 0 ms (100.00%) | Memory: 49.2 MB (63.73%) - LeetSync

---
+++ 75-sort-colors/sort-colors.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    for(let i=1;i<nums.length;i++){
+        key=nums[i];
+        let j=i-1;
+        while(j>=0 && nums[j]>key){
+            nums[j+1]=nums[j];
+            j--;
+        }
+        nums[j+1]=key
+    }
+};
