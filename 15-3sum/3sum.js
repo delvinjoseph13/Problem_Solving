22dd772265cc2cc40ca0e98d521d588490a59dcd
Delvin joseph <124490000+delvinjoseph13@users.noreply.github.com>
2024-12-23 10:43:30 +0530

    Time: 53 ms (24.06%) | Memory: 65.6 MB (70.38%) - LeetSync

---
+++ 15-3sum/3sum.js
@@ -0,0 +1,49 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+  const result=[];
+
+    for(let i=0;i<nums.length;i++){
+        let min_index=i;
+
+        for(let j=i+1;j<nums.length;j++){
+            if(nums[j]<nums[min_index]){
+                min_index=j;
+            }
+        }
+            let temp=nums[min_index];
+            nums[min_index]=nums[i];
+            nums[i]=temp;
+        
+    }
+    
+    for(let i=0;i<nums.length-2;i++){
+
+        if(i>0 && nums[i]==nums[i-1]) continue;
+
+        let left=i+1;
+        let right=nums.length-1;
+
+        while(left<right){
+            const sum=nums[i]+nums[left]+nums[right];
+
+            if(sum==0){
+                result.push([nums[i],nums[left],nums[right]])
+
+                while(left<right && nums[left]===nums[left+1]) left++;
+                while(left<right && nums[right]===nums[right-1]) right--;
+                left++;
+                right--;
+            }
+           else if(sum<0){
+                left++;
+            }
+            else {
+                right --;
+            }
+        }
+    }
+    return result;
+};
