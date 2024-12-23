0ef4029d09e6efdfdc11864c1c4cc8d84cf14049
Delvin joseph <124490000+delvinjoseph13@users.noreply.github.com>
2024-12-23 12:43:25 +0530

    Time: 0 ms (100.00%) | Memory: 48.8 MB (60.79%) - LeetSync

--- 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.js
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.js
@@ -5,12 +5,12 @@
 //  TC-O(n);
 //  SC-O(1);
 var maxChunksToSorted = function(arr) {
-    let maxSoFar=0;
+    let maxElement=0;
     let chunks=0;
 
     for(let i=0;i<arr.length;i++){
-        maxSoFar=Math.max(arr[i],maxSoFar);
-        if(maxSoFar==i){
+        maxElement=Math.max(arr[i],maxElement);
+        if(maxElement==i){
             chunks++;
         }
     }
