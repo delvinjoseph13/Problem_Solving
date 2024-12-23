d692ce6b84268631b14385b5c99e8a82c1f95669
Delvin joseph <124490000+delvinjoseph13@users.noreply.github.com>
2024-12-23 09:55:57 +0530

    Time: 2 ms (97.19%) | Memory: 65 MB (43.95%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.js
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -3,20 +3,19 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-     let n=nums.length
-     let answer=new Array(n).fill(1);
+    let n=nums.length;
+    let answer=new Array(n).fill(1);
 
-     let prefix=1;
-     for(let i=0;i<nums.length;i++){
+    let prefix=1;
+    for(let i=0;i<n;i++){
         answer[i]=prefix;
         prefix*=nums[i];
-     }
+    }
 
-
-     let suffix=1;
-     for(let i=n-1;i>=0;i--){
+    let suffix=1;
+    for(let i=n-1;i>=0;i--){
         answer[i]*=suffix;
-        suffix*=nums[i];
-     }
-     return answer
+        suffix*=nums[i]
+    }
+    return answer;
 };
