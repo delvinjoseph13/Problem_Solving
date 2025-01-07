3a6fbfc70d6d781c6e9f8dd280ea3c3bcfd38462
Delvin joseph <124490000+delvinjoseph13@users.noreply.github.com>
2025-01-07 17:42:06 +0530

    Time: 1 ms (89.78%) | Memory: 51.5 MB (28.17%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,19 +4,15 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let map=new Map();
-    let ansarray=[]
-    for(let i=0;i<nums.length;i++){
-        let a=nums[i];
-        let b=target-a;
-
-        if(map.has(b)==true){
-            ansarray.push(map.get(b),i)
-        }
-      else{
-         map.set(nums[i],i);
-      }  
+let map=new Map();
+let ans=[];
+for(let i=0;i<nums.length;i++){
+    let a=nums[i];
+    let b=target-a;
+    if(map.has(b)){
+        ans.push(map.get(b),i)
     }
-    return ansarray;
-    
+    map.set(nums[i],i)
+}
+   return ans; 
 };
