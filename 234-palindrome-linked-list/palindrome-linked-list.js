9bf8b843582347cbccbb28feecc9b883cbf7520c
Delvin joseph <124490000+delvinjoseph13@users.noreply.github.com>
2025-01-22 23:00:37 +0530

    Time: 10 ms (52.41%) | Memory: 69.5 MB (81.83%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+if (!head || !head.next) {
+        return true; // An empty list or a single-node list is always a palindrome
+    } 
+   let fast=head;
+   let slow=head;
+   while(fast!=null && fast.next!=null){
+    fast=fast.next.next;
+    slow=slow.next
+   }
+    
+    let prev=null;
+    let next=null;
+    while(slow!=null){
+        next=slow.next;
+        slow.next=prev;
+        prev=slow;
+        slow=next;
+    }
+    
+    let left=head;
+    let right=prev;
+
+    while(right!=null){
+        if(left.val!==right.val){
+            return false;
+        }
+            left=left.next;
+            right=right.next
+    }
+    return true;
+};
