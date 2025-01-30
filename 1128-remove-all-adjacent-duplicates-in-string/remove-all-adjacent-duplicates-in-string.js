a2f0b23cd65447d5a632ccd604c57ee205a0c8b6
Delvin joseph <124490000+delvinjoseph13@users.noreply.github.com>
2025-01-30 09:07:42 +0530

    Time: 10 ms (85.02%) | Memory: 58.4 MB (27.84%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.js
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.js
@@ -6,10 +6,10 @@ var removeDuplicates = function(s) {
     let stack=[];
     for(const char of s){
         if(stack.length>0 && stack[stack.length-1]===char){
-            stack.pop();
-        }else{
-            stack.push(char)
-        }
+            stack.pop()
+        }else
+        stack.push(char);
     }
-    return stack.join('')
+    return stack.join('');
 };
+
